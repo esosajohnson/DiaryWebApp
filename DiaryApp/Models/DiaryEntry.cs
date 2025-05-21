4bb3438b12bf02e992c1e33c434acc287cfeb0f5
Esosa <ikponmwose@coventry.ac.uk>
2025-05-21 19:25:29 +0100

    Property creation for the DiaryEntry class

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        // Properties for the DiaryEntry class
+        public int Id { get; set; }
+        [Required]
+        public required string Title { get; set; }
+        [Required]
+        public required string Description { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
